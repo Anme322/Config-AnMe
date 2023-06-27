9b3fa39a8da2a7b1c3aa2fccbc2c800f1f755d35
Anme322 <116656887+Anme322@users.noreply.github.com>
2023-06-27 22:31:27 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import "./index.css";
+import MY_IMAGE from "./assets/photo.png";
+
+
+const img = document.createElement("img");
+img.src = MY_IMAGE;
+document.body.append(img);
+console.log('Hello World!');
